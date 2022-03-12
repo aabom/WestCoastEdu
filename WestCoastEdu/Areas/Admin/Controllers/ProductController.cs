2004721d1e49a69f4cbfb00ad0695dc7585e788a
aabom <tobias.abom@gmail.com>
2022-03-12 21:40:04 +0100

    Started with first api call

--- WestCoastEdu/Areas/Admin/Controllers/ProductController.cs
+++ WestCoastEdu/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using WestCoastEdu.DataAccess.Repository.IRepository;
 using WestCoastEdu.Models;
@@ -9,15 +10,16 @@ namespace WestCoastEdu.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Product> productList = _unitOfWork.Product.GetAll();
-            return View(productList);
+            return View();
         }
 
         //GET
@@ -41,22 +43,90 @@ namespace WestCoastEdu.Areas.Admin.Controllers
             if (id is null or 0)
             {
                 //create product
-                //ViewBag.CategoryList = CategoryList;
-                //ViewData["CoverTypeList"] = CoverTypeList;
+                //ViewBag.LocationList = LocationList;
+                //ViewData["StatusList"] = StatusList;
                 return View(productVM);
             }
             else
             {
-                productVM.Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
-                return View(productVM);
 
                 //update product
             }
-
-
+            return View(productVM);
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM obj, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRothPath = _webHostEnvironment.WebRootPath;
+                if (file is not null)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRothPath, @"images\products");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    using (var fileStreams =
+                           new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        file.CopyTo(fileStreams);
+                    }
+
+                    obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
+                }
+                _unitOfWork.Product.Add(obj.Product);
+                _unitOfWork.Save();
+                TempData["success"] = "Course Created Successfully";
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id is null or 0)
+            {
+                return NotFound();
+            }
+            var product = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var product = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == id);
+            if (id is null or 0)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Course Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var productList = _unitOfWork.Product.GetAll();
+            return Json(new {data = productList});
+        }
         
+
+        #endregion
 
     }
 }
