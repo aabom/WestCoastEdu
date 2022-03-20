c136942f93b247742cbf3b468680a13683c23803
aabom <tobias.abom@gmail.com>
2022-03-20 21:20:24 +0100

    Changed using in OrderDetail preventing migration

--- WestCoastEdu.DataAccess/Migrations/20220320195956_addOrderDetailsToDb.Designer.cs
+++ WestCoastEdu.DataAccess/Migrations/20220320195956_addOrderDetailsToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WestCoastEdu.DataAccess;
 
@@ -11,9 +12,10 @@ using WestCoastEdu.DataAccess;
 namespace WestCoastEdu.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220320195956_addOrderDetailsToDb")]
+    partial class addOrderDetailsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -284,35 +286,6 @@ namespace WestCoastEdu.DataAccess.Migrations
                     b.ToTable("Location");
                 });
 
-            modelBuilder.Entity("WestCoastEdu.Models.OrderDetail", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
-
-                    b.Property<int>("Count")
-                        .HasColumnType("int");
-
-                    b.Property<int>("OrderId")
-                        .HasColumnType("int");
-
-                    b.Property<double>("Price")
-                        .HasColumnType("float");
-
-                    b.Property<int>("ProductId")
-                        .HasColumnType("int");
-
-                    b.HasKey("Id");
-
-                    b.HasIndex("OrderId");
-
-                    b.HasIndex("ProductId");
-
-                    b.ToTable("OrderDetails");
-                });
-
             modelBuilder.Entity("WestCoastEdu.Models.OrderHeader", b =>
                 {
                     b.Property<int>("Id")
@@ -555,25 +528,6 @@ namespace WestCoastEdu.DataAccess.Migrations
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
-                });
-
-            modelBuilder.Entity("WestCoastEdu.Models.OrderDetail", b =>
-                {
-                    b.HasOne("WestCoastEdu.Models.OrderHeader", "OrderHeader")
-                        .WithMany()
-                        .HasForeignKey("OrderId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("WestCoastEdu.Models.Product", "Product")
-                        .WithMany()
-                        .HasForeignKey("ProductId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("OrderHeader");
-
-                    b.Navigation("Product");
                 });
 
             modelBuilder.Entity("WestCoastEdu.Models.OrderHeader", b =>
--- WestCoastEdu.Models/OrderDetail.cs
+++ WestCoastEdu.Models/OrderDetail.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using Microsoft.Build.Framework;
 
 namespace WestCoastEdu.Models
 {
