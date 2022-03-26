3e50c46e51837bf538d3ab60530b45333a3e7d90
aabom <tobias.abom@gmail.com>
2022-03-26 18:36:56 +0100

    Added api methods to product controller

--- WCE.API/Controllers/ProductController.cs
+++ WCE.API/Controllers/ProductController.cs
@@ -8,7 +8,7 @@ namespace WCE.API.Controllers
     [ApiController]
     [Route("[controller]")]
     public class ProductController : ControllerBase
-    {        
+    {
         private readonly ILogger<ProductController> _logger;
         private readonly ApplicationDbContext _applicationDbContext;
 
@@ -18,22 +18,59 @@ namespace WCE.API.Controllers
             _applicationDbContext = applicationDbContext;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<Product> GetProducts()
+        [HttpGet]
+        public IEnumerable<Product> GetAll()
         {
             return _applicationDbContext.Products.ToList();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> Get(int id)
+        public async Task<ActionResult<Product>> GetById(int id)
         {
             var product = await _applicationDbContext.Products.FindAsync(id);
             if (product == null)
             {
-                return BadRequest("Product not found");
+                return NotFound($"Product with id:{id} not found");
             }
             return Ok(product);
         }
-        
+
+        [HttpPost]
+        public async Task<ActionResult<Product>> AddProduct(Product product)
+        {
+            if (!ModelState.IsValid || product == null)
+            {
+                return BadRequest();
+            }
+
+            _applicationDbContext.Products.Add(product);
+            await _applicationDbContext.SaveChangesAsync();
+            return Ok("Product added successfully");
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Product>> UpdateProduct(Product product)
+        {
+            var dbProduct = await _applicationDbContext.Products.FindAsync(product.Id);
+            if(dbProduct == null)
+            {
+                return BadRequest("Product not found");
+            }
+
+            dbProduct.Title = product.Title;
+            dbProduct.Description = product.Description;
+            dbProduct.Length = product.Length;
+            dbProduct.Level = product.Level;
+            dbProduct.Price = product.Price;
+            dbProduct.ImageUrl = product.ImageUrl;
+            dbProduct.LocationId = product.LocationId;
+            dbProduct.StatusId = product.StatusId;
+
+            await _applicationDbContext.SaveChangesAsync();
+            return Ok("Product updated successfully");
+
+        }
+
+
     }
 }
